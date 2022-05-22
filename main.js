3b61b84e13f5e22ea1dc7a451824095530f2f11b
Kanaknakola <jakub.pecak@o2.pl>
2022-05-22 13:18:21 +0200

    add

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 const taskNumber = document.querySelector("h1 span");
-const listItems = document.querySelectorAll("li.task");
+const listItems = document.getElementsByClassName("task");
 const input = document.querySelector("input");
+
+const removeTasks = (e) => {
+   e.target.parentNode.remove();
+   taskNumber.textContent = listItems.length;
+}
 
 const addTask = (e) => {
    e.preventDefault();
    const titleTask = input.value;
    if (titleTask === "") {
-      return
+      return;
    }
    const task = document.createElement("li");
    task.className = "task";
    task.innerHTML = `${titleTask} <button>usuń</button>`;
    ul.appendChild(task);
    input.value = "";
-   
-
+   taskNumber.textContent = listItems.length;
+   task.querySelector("button").addEventListener('click', removeTasks);
 
 }
 
